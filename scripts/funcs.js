990f46346bf71a4cabe2794b80a3a13a0173d3bc
taravannheng <heng.t.taravann@gmail.com>
2021-04-23 18:02:14 +0700

    Add modifiers for add to cart button

--- scripts/funcs.js
+++ scripts/funcs.js
@@ -1,28 +1,46 @@
 function createCarEl(car) {
-    return $(`<div class="main__card card" id="${car.id}">
-                <div class="card__img" style="background-image: url(${car.image})"></div>
-                <h2 class="card__title"><span class="card__title-brand">${car.brand}</span> <span class="card__title-model">${car.model}</span> <span class="card__title-model-year">${car.modelYear}</span></h2>
-                <div class="card__details">
-                    <ul class="card__details-labels">
-                        <li class="card__details-labels-mileage">Mileage: </li>
-                        <li class="card__details-labels-fuel-type">Fuel Type: </li>
-                        <li class="card__details-labels-seat">Seats: </li>
-                        <li class="card__details-labels-price-per-day">Price Per Day: </li>
-                        <li class="card__details-labels-availability">Availability: </li>
-                    </ul>
-                    <ul class="card__details-texts">
-                        <li class="card__details-texts-mileage">${Number(car.mileage).toLocaleString()} kms</li>
-                        <li class="card__details-texts-fuel-type">${car.fuelType}</li>
-                        <li class="card__details-texts-seat">${car.seats}</li>
-                        <li class="card__details-texts-price-per-day">$${car.pricePerDay}</li>
-                        <li class="card__details-texts-availability">${car.availability}</li>
-                    </ul>
-                </div>
-                <div class="card__description">
-                    <p class="card__description-text">${car.description}</p>
-                </div>
-                <button type="button" class="card__button button"><span class="button__text">Add to Cart</span></button>
-            </div>`);
+    const cardEl = $(`<div class="main__card card" id="${car.id}"></div>`);
+    const cardImgEl = $(`<div class="card__img" style="background-image: url(${car.image})"></div>`);
+    const cardTitleEl = $(`<h2 class="card__title"><span class="card__title-brand">${car.brand}</span> <span class="card__title-model">${car.model}</span> <span class="card__title-model-year">${car.modelYear}</span></h2>`);
+    const cardDetailsEl = $(`
+        <div class="card__details">
+            <ul class="card__details-labels">
+                <li class="card__details-labels-mileage">Mileage: </li>
+                <li class="card__details-labels-fuel-type">Fuel Type: </li>
+                <li class="card__details-labels-seat">Seats: </li>
+                <li class="card__details-labels-price-per-day">Price Per Day: </li>
+                <li class="card__details-labels-availability">Availability: </li>
+            </ul>
+            <ul class="card__details-texts">
+                <li class="card__details-texts-mileage">${Number(car.mileage).toLocaleString()} kms</li>
+                <li class="card__details-texts-fuel-type">${car.fuelType}</li>
+                <li class="card__details-texts-seat">${car.seats}</li>
+                <li class="card__details-texts-price-per-day">$${car.pricePerDay}</li>
+                <li class="card__details-texts-availability">${car.availability}</li>
+            </ul>
+        </div>
+    `);
+    const cardDescriptionEl = $(`
+        <div class="card__description">
+            <p class="card__description-text">${car.description}</p>
+        </div>
+    `);
+
+    let cardButtonEl;
+
+    if (car.availability === 'false') {
+        cardButtonEl = $(`<button type="button" class="card__button card__button--unavailable button"><span class="button__text">Not Available</span></button>`);
+    } else {
+        cardButtonEl = $(`<button type="button" class="card__button button"><span class="button__text">Add to Cart</span></button>`);
+    }
+
+    cardEl.append(cardImgEl);
+    cardEl.append(cardTitleEl);
+    cardEl.append(cardDetailsEl);
+    cardEl.append(cardDescriptionEl);
+    cardEl.append(cardButtonEl);
+
+    return cardEl;
 }
 
 function loadCars() {
---
+++ scripts/vars.js
@@ -0,0 +1,2 @@
+let cars;
+let selectedCars = [];
